66b5f95c810150b80f892033064c33d57b8fac41
vjdeshmukhgit <vijay@intelifx.com>
2016-07-30 13:33:19 -0400

    Updates to script

--- HWorld2Project/BuildExec/BasicPSScript3.ps1
+++ HWorld2Project/BuildExec/BasicPSScript3.ps1
@@ -1,3 +1,20 @@
 ﻿Import-Module -Name "C:\MyWork\Self\VSProject\HWorld2\H-World-Project-Rep\HWorld2Project\BuildExec\Invoke-MsBuild.psm1"
 
 Write-Host "module imported!"
+
+$outputDir = "C:\MyWork\Self\VSProject\HWorld2\H-World-Project-Rep\HWorld2Project\PublishedBuild"
+
+Write-Host "Publish build to $outputDir" 
+
+$BuildSucceeded = Invoke-MSBuild -Path "C:\MyWork\Self\VSProject\HWorld2\H-World-Project-Rep\HWorld2Project\HWorld2Project.sln" -MsBuildParameters "/t:rebuild /p:outdir=C:\MyWork\Self\VSProject\HWorld2\H-World-Project-Rep\HWorld2Project\PublishedBuild\" -BuildLogDirectoryPath "C:\MyWork\Self\VSProject\HWorld2\H-World-Project-Rep\HWorld2Project\BuildExec\Log" -KeepBuildLogOnSuccessfulBuilds
+
+if($BuildSucceeded.BuildSucceeded -eq $true)
+{
+    Write-Output "Build process success:"
+    
+ }
+else
+{
+    Write-Output "Build process failed:"
+    }
+
