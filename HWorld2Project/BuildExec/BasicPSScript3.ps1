5be260d4d7fc2934d00587384735b174ded1c929
vjdeshmukhgit <vijay@intelifx.com>
2016-07-30 13:36:34 -0400

    All pointing to Buildexec folder only

--- HWorld2Project/BuildExec/BasicPSScript3.ps1
+++ HWorld2Project/BuildExec/BasicPSScript3.ps1
@@ -2,11 +2,11 @@
 
 Write-Host "module imported!"
 
-$outputDir = "C:\MyWork\Self\VSProject\HWorld2\H-World-Project-Rep\HWorld2Project\PublishedBuild"
+$outputDir = "C:\MyWork\Self\VSProject\HWorld2\H-World-Project-Rep\HWorld2Project\BuildExec"
 
 Write-Host "Publish build to $outputDir" 
 
-$BuildSucceeded = Invoke-MSBuild -Path "C:\MyWork\Self\VSProject\HWorld2\H-World-Project-Rep\HWorld2Project\HWorld2Project.sln" -MsBuildParameters "/t:rebuild /p:outdir=C:\MyWork\Self\VSProject\HWorld2\H-World-Project-Rep\HWorld2Project\PublishedBuild\" -BuildLogDirectoryPath "C:\MyWork\Self\VSProject\HWorld2\H-World-Project-Rep\HWorld2Project\BuildExec\Log" -KeepBuildLogOnSuccessfulBuilds
+$BuildSucceeded = Invoke-MSBuild -Path "C:\MyWork\Self\VSProject\HWorld2\H-World-Project-Rep\HWorld2Project\HWorld2Project.sln" -MsBuildParameters "/t:rebuild /p:outdir=C:\MyWork\Self\VSProject\HWorld2\H-World-Project-Rep\HWorld2Project\BuildExec\" -BuildLogDirectoryPath "C:\MyWork\Self\VSProject\HWorld2\H-World-Project-Rep\HWorld2Project\BuildExec" -KeepBuildLogOnSuccessfulBuilds
 
 if($BuildSucceeded.BuildSucceeded -eq $true)
 {
