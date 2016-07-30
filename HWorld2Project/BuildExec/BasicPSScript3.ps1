a08da5e1f3decd7ae38040d436995409835d535c
vjdeshmukhgit <vijay@intelifx.com>
2016-07-30 12:52:30 -0400

    Updated path with parent dir

--- HWorld2Project/BuildExec/BasicPSScript3.ps1
+++ HWorld2Project/BuildExec/BasicPSScript3.ps1
@@ -1,3 +1,3 @@
-﻿Import-Module -Name "HWorld2Project\BuildExec\Invoke-MsBuild.psm1"
+﻿Import-Module -Name "C:\MyWork\Self\VSProject\HWorld2\H-World-Project-Rep\HWorld2Project\BuildExec\Invoke-MsBuild.psm1"
 
 Write-Host "module imported!"
