3a53017b67c08cb31e4f2af9753c3701436f3208
vjdeshmukhgit <vijay@intelifx.com>
2016-08-01 16:27:08 -0400

    # Please include a useful commit message!

--- HWorld2Project/HWorld2Project/Program.cs
+++ HWorld2Project/HWorld2Project/Program.cs
@@ -10,7 +10,7 @@ namespace HWorld2Project
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world! Enter your First and last name: " );
+            Console.WriteLine("Hello world! Enter your name 1: " );
             var key = Console.ReadLine();
             Console.WriteLine("You entered " + key);
 
